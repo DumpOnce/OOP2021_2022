001482c2c8566668d1b1f9cf2f6553215d88c05a
Никита Сазиков <sazikov.nd@noreply.vcs.dozen.mephi.ru>
2021-12-28 19:58:12 +0000

    Загрузил(а) файлы в '3/AirBattleGame'

---
+++ 3/AirBattleGame/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "Game/AirBattle.hpp"
+#include <windows.h>
+#include <cwchar>
+
+int main()
+{
+	HANDLE soh = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_FONT_INFOEX cfi;
+	memset(&cfi, 0, sizeof(cfi));
+	cfi.cbSize = sizeof(cfi);
+	GetCurrentConsoleFontEx(soh, FALSE, &cfi);
+	cfi.dwFontSize.Y += 7;
+	SetCurrentConsoleFontEx(soh, FALSE, &cfi);
+	try {
+		AirBattle Game;
+	}
+	catch (...) {
+		std::cerr << "Unknown error!";
+	}
+	return 0;
+}
